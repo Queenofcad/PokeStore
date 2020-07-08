1eda232a6b4ec2c354c66879ae04564ab6c806f0
Queenofcad <62190908+Queenofcad@users.noreply.github.com>
2020-07-08 14:50:24 -0300

    ultimo

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,6 @@
 import { createStore } from 'redux';
 import rootReducer from './modules/rootReduces';
 
-// const enhancer =
-//   process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
 
 const store = createStore(rootReducer);
 
